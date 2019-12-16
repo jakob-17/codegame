e34ca0627435578f5e8b8ce50964830ca42ead8d
Jakob Horvath <horvath.j17@gmail.com>
2019-12-15 16:09:48 -0800

    initial bit of work

---
+++ Assets/Scripts/CreateGrid.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGrid : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1f;
+
+    List<Vector3> points = new List<Vector3>();
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+        int zCount = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3((float)xCount * size, (float)yCount * size, (float)zCount * size);
+
+        result += transform.position - new Vector3(4f, 0f, 4f);
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        for (float x = 0; x < 9; x += size)
+        {
+            for (float z = 0; z < 9; z += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.05f);
+
+                points.Add(point);
+            }
+        }
+        // draw vertical lines
+        for (int i = 0; i < 81; i += 9)
+        {
+            for (int k = 1 * i; k < 1 * i + 9 - 1; k++)
+            {
+                Gizmos.DrawLine(points[k], points[k + 1]);
+            }
+        }
+        // draw horizontal lines
+        for (int i = 0; i < 81; i += 9)
+        {
+            for (int k = 1 * i; k < 1 * i + 9; k++)
+            {
+                Gizmos.DrawLine(points[k], points[k + 9]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float size = 1f;
+
+    public CreateGrid cg;
+
+    public float speed = 1.0f;
+
+    private void Update()
+    {
+        Vector3 pos = transform.position;
+        float step = speed * Time.deltaTime; // calculate distance to move
+        Vector3 target;
+
+        if (Input.GetKey("w"))
+        {
+            target = GetNearestPointOnGrid(transform.position + new Vector3(0f, 0f, 1f));
+        }
+        if (Input.GetKey("s"))
+        {
+            target = GetNearestPointOnGrid(transform.position + new Vector3(0f, 0f, -1f));
+        }
+        if (Input.GetKey("d"))
+        {
+            target = GetNearestPointOnGrid(transform.position + new Vector3(1f, 0f, 0f));
+        }
+        if (Input.GetKey("a"))
+        {
+            target = GetNearestPointOnGrid(transform.position + new Vector3(-1f, 0f, 0f));
+        }
+
+        // use Vector3.MoveTowards to move to node ...
+
+        transform.position = Vector3.MoveTowards(transform.position, , step);
+    }
+
+    /// <summary>
+    /// Copied from CreateGrid class.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / size);
+        int yCount = Mathf.RoundToInt(position.y / size);
+        int zCount = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3((float)xCount * size, (float)yCount * size, (float)zCount * size);
+
+        result += transform.position - new Vector3(4f, 0f, 4f);
+
+        return result;
+    }
+}
