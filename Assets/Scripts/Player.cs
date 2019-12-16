0f4696bba40e74d73cb58a1f3252483a56eac2d2
Jakob Horvath <horvath.j17@gmail.com>
2019-12-15 16:39:18 -0800

    the player can move 1 space... progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,58 +4,36 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private float size = 1f;
-
     public CreateGrid cg;
 
     public float speed = 1.0f;
 
+    private bool playersTurn = true;
+
     private void Update()
     {
         Vector3 pos = transform.position;
-        float step = speed * Time.deltaTime; // calculate distance to move
-        Vector3 target;
 
-        if (Input.GetKey("w"))
+        if (playersTurn && Input.anyKey)
         {
-            target = GetNearestPointOnGrid(transform.position + new Vector3(0f, 0f, 1f));
+            if (Input.GetKey("w"))
+            {
+                transform.position = pos + Vector3.forward;
+            }
+            else if (Input.GetKey("s"))
+            {
+                transform.position = pos + Vector3.back;
+            }
+            else if (Input.GetKey("d"))
+            {
+                transform.position = pos + Vector3.right;
+            }
+            else if (Input.GetKey("a"))
+            {
+                transform.position = pos + Vector3.left;
+            }
+
+            playersTurn = false;
         }
-        if (Input.GetKey("s"))
-        {
-            target = GetNearestPointOnGrid(transform.position + new Vector3(0f, 0f, -1f));
-        }
-        if (Input.GetKey("d"))
-        {
-            target = GetNearestPointOnGrid(transform.position + new Vector3(1f, 0f, 0f));
-        }
-        if (Input.GetKey("a"))
-        {
-            target = GetNearestPointOnGrid(transform.position + new Vector3(-1f, 0f, 0f));
-        }
-
-        // use Vector3.MoveTowards to move to node ...
-
-        transform.position = Vector3.MoveTowards(transform.position, , step);
-    }
-
-    /// <summary>
-    /// Copied from CreateGrid class.
-    /// </summary>
-    /// <param name="position"></param>
-    /// <returns></returns>
-    public Vector3 GetNearestPointOnGrid(Vector3 position)
-    {
-        position -= transform.position;
-
-        int xCount = Mathf.RoundToInt(position.x / size);
-        int yCount = Mathf.RoundToInt(position.y / size);
-        int zCount = Mathf.RoundToInt(position.z / size);
-
-        Vector3 result = new Vector3((float)xCount * size, (float)yCount * size, (float)zCount * size);
-
-        result += transform.position - new Vector3(4f, 0f, 4f);
-
-        return result;
     }
 }
